e48527eb0219791660822244e263e8813641db05
vitorYghor <devictoryghor@gmail.com>
2024-09-24 23:20:53 -0300

    Connect with aws to upload the backup

--- backup/views.py
+++ backup/views.py
@@ -1,3 +1,4 @@
+import os
 from cgi import parse_header
 from lib2to3.pgen2.parse import ParseError
 
@@ -8,16 +9,21 @@ from rest_framework import views, status
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from liberaNuvem import settings
+import boto3
 
 class FileUploadView(views.APIView):
     parser_classes = (FileUploadParser, )
     media_type = "*/*"
     def post(self, request, *args, **kwargs):
+        if request.headers['api-key'] != os.environ['API_KEY']:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
         if 'file' not in request.FILES:
             return Response({'detail': 'No file provided'}, status=status.HTTP_400_BAD_REQUEST)
-        file_uploaded = request.FILES['file']
 
+        file_uploaded = request.FILES['file']
+        s3 = boto3.resource('s3')
+        s3.Bucket('liberanuvem').put_object(Key='backup.zip', Body=file_uploaded)
 
         # Aqui você pode fazer algo com o arquivo recebido
-        return HttpResponse()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
